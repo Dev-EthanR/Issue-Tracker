db4e04575b194ef23b0bda6622ccbd722541f271
Ethan <ethanlegend7@gmail.com>
2026-02-10 13:59:36 +1100

    Build the issue Status badge

---
+++ app/components/IssueStatusBadege.tsx
@@ -0,0 +1,23 @@
+import { Status } from "@/generated/prisma/enums";
+import { Badge } from "@radix-ui/themes";
+
+interface Props {
+  status: Status;
+}
+
+const statusMap: Record<
+  Status,
+  { label: string; color: "red" | "violet" | "green" }
+> = {
+  OPEN: { label: "Open", color: "red" },
+  IN_PROGRESS: { label: "In Progress", color: "violet" },
+  CLOSED: { label: "Closed", color: "green" },
+};
+
+const IssueStatusBadege = ({ status }: Props) => {
+  return (
+    <Badge color={statusMap[status].color}>{statusMap[status].label}</Badge>
+  );
+};
+
+export default IssueStatusBadege;
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Button, Table } from "@radix-ui/themes";
 import Link from "next/link";
 import { prisma } from "@/prisma/client";
+import IssueStatusBadege from "../components/IssueStatusBadege";
 
 const IssuesPage = async () => {
   const issues = await prisma.issue.findMany();
@@ -29,10 +30,12 @@ const IssuesPage = async () => {
             <Table.Row key={issue.id}>
               <Table.Cell>
                 {issue.title}
-                <div className="block md:hidden">{issue.status}</div>
+                <div className="block md:hidden">
+                  <IssueStatusBadege status={issue.status} />
+                </div>
               </Table.Cell>
               <Table.Cell className="hidden md:table-cell">
-                {issue.status}
+                <IssueStatusBadege status={issue.status} />
               </Table.Cell>
               <Table.Cell className="hidden md:table-cell">
                 {issue.createdAt.toDateString()}
