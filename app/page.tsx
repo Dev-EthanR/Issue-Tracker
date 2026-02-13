6e88cd20b297f0348b2460dabc9571546654b7fa
Ethan <ethanlegend7@gmail.com>
2026-02-13 16:37:25 +1100

    Build the latest issue component

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,9 @@
-import Pagination from "./components/Pagination";
+import LatestIssues from "./LatestIssues";
 
-export default async function Home({
-  searchParams,
-}: {
-  searchParams: { page: string };
-}) {
-  const { page } = await searchParams;
+export default async function Home() {
   return (
     <div>
-      <Pagination itemCount={100} pageSize={10} currentPage={parseInt(page)} />
+      <LatestIssues />
     </div>
   );
 }
